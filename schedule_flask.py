ffe81441aa6df3e70f4221aa438ad470ab649e6b
Denis (@denismv) <devamax@mail.ru>
2017-01-21 17:21:57 +0300

    Добавил в интерфейс возможность добавления и удаления пользователей

--- schedule_flask.py
+++ schedule_flask.py
@@ -46,7 +46,6 @@ schedule_app = Flask(__name__)
 def index():
     date = datetime.date.today()
 #    date = datetime.date(date.year, date.month, 1)
-#    date = datetime.date(date.year, 2, 1)
     schedule_days=yaml.load(schedule_maker_if.cmd_show_schedule(int(date.strftime("%Y%m"))))
     schedule_month = process_month(date)
 
@@ -57,23 +56,28 @@ def index():
                             schedule_days = schedule_days)
     return result
 
-#@schedule_app.route('/useradd', methods=['POST'])
-#def useradd():
-#    return render_template('useradd.html', user=request.form.get('user'))
+@schedule_app.route('/useradd', methods=['GET', 'POST'])
+def useradd():
+    useradd_result=schedule_maker_if.cmd_add_user(request.form.get('user'))
+    if useradd_result == 'ok':
+        display_message='Пользователь ' + request.form.get('user') + ' успешно добавлен'
+        # schedule_maker_if.cmd_make_schedule(int(datetime.date.today().strftime("%Y%m")))
+        schedule_maker_if.cmd_rebuild()
+    else:
+        display_message='Ошибка добавления пользователя'
 
-@schedule_app.route('/test')
-def mytest():
-    date = datetime.date.today()
-#    date = datetime.date(date.year, date.month, 1)
-    schedule_days=yaml.load(schedule_maker_if.cmd_show_schedule(int(date.strftime("%Y%m"))))
-    schedule_month = process_month(date)
+    return render_template('user.html', message=display_message)
 
-    result = render_template("test.html",
-                            month = schedule_month['month'],
-                            year = schedule_month['year'],
-                            days = schedule_month['days'],
-                            schedule_days = schedule_days)
-    return result
+@schedule_app.route('/userdel', methods=['GET', 'POST'])
+def userdel():
+    useradd_result=schedule_maker_if.cmd_del_user(request.form.get('user'))
+    if useradd_result == 'ok':
+        display_message='Пользователь ' + request.form.get('user') + ' успешно удален'
+        schedule_maker_if.cmd_rebuild()
+    else:
+        display_message='Ошибка удаления пользователя'
+
+    return render_template('user.html', message=display_message)
 
 if __name__ == "__main__":
     setup_locale(sys.platform)
